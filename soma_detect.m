3954b774490eee24d97fe34cd76b2c9739e6eb2e
Grace Seungin Yoo <yoo22g@mtholyoke.edu>
2015-01-16 11:34:41 -0500

    start_prog, soma_detect, STUBS

---
+++ soma_detect.m
@@ -0,0 +1,55 @@
+function [ output_args ] = Untitled3( input_args )
+%UNTITLED3 Summary of this function goes here
+%   %% soma detection for biomath bootcamp project
+% code adapted from SynD
+%
+% Johannes Hjorth
+% johannes.hjorth@cncr.vu.nl
+% hjorth@kth.se
+%
+% Sabine Schmitz
+% sabine.schmitz@cncr.vu.nl
+%
+% The latest version of the code is available from:
+% http://www.johanneshjorth.se/SynD
+% 
+% You can also find the code at:
+%
+% http://software.incf.org/software/synd
+% http://www.cncr.nl/resources
+%
+% Schmitz SK, Hjorth JJJ, Joemai RMS, Wijntjes R, Eijgenraam S,  
+% de Bruijn P, Georgiou C, de Jong APH, van Ooyen A, Verhage M, 
+% Cornelisse LN, Toonen RF, Veldkamp W 
+% Automated Analysis of Neuronal Morphology, Synapse Number and 
+% Synaptic Recruitment, Journal of Neuroscience Methods 195 (2011) 185–193
+% DOI: 10.1016/j.jneumeth.2010.12.011
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+  % SynD can read in tiff-stacks containing multiple images, but it only
+  % processes the first image. The dispInfo.curImage is a legacy from when
+  % the program allowed the user to select which of the images in a stack
+  % to use. The functionality can be reimplemented, since all other functions
+  % uses dispInfo.curImage to extract the relevant image to use.
+  %
+  % The soma is defined by data.somaMask which is data.height x data.width
+  % in size, pixels belonging to the soma are set to 1, the others are 0.
+  % More than one soma is allowed in the mask. In addition there is a 
+  % data.somaMeasureMask that optionally defines circular regions of interest
+  % in the soma that can be specified by the user. These ROIs are derived from
+  % data.measurePoint which specifies the pixel index of the center of the
+  % ROIs.
+  
+%   %The soma is separated from
+% connected neurites by performing a morphological opening (erosion
+% followed by dilation) using a disk with radius 15 pixels as
+% the structural element (Supplementary Fig. 1D and E).
+
+%
+%
+
+
+end
+
---
+++ start_prog.m
@@ -0,0 +1,44 @@
+%% load image and run program
+% biomath bootcamp project
+% 
+%
+% code adapted from SynD
+%
+% Johannes Hjorth
+% johannes.hjorth@cncr.vu.nl
+% hjorth@kth.se
+%
+% Sabine Schmitz
+% sabine.schmitz@cncr.vu.nl
+%
+% The latest version of the code is available from:
+% http://www.johanneshjorth.se/SynD
+% 
+% You can also find the code at:
+%
+% http://software.incf.org/software/synd
+% http://www.cncr.nl/resources
+%
+% Schmitz SK, Hjorth JJJ, Joemai RMS, Wijntjes R, Eijgenraam S,  
+% de Bruijn P, Georgiou C, de Jong APH, van Ooyen A, Verhage M, 
+% Cornelisse LN, Toonen RF, Veldkamp W 
+% Automated Analysis of Neuronal Morphology, Synapse Number and 
+% Synaptic Recruitment, Journal of Neuroscience Methods 195 (2011) 185–193
+% DOI: 10.1016/j.jneumeth.2010.12.011
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+% read the sample image
+% using original 1024 x 1024 px image WILL cause system to CRASH
+I = imread('images/1.jpg');
+save I
+load I
+%imshow(I)
+
+% convert to binary, with threshold
+threshold = 0.1;
+BW = im2bw(I, threshold);
+% show figres
+imshow(I), figure, imshow(BW)
